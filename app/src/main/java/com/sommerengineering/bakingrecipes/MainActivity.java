bc3d481c5ff77b45b86d46bbc6be6bb81518aa9d
Drew Sommer <andrew.p.sommer@gmail.com>
2019-02-08 15:28:07 -0800

    Helper method to check status of internet connectivity

--- app/src/main/java/com/sommerengineering/bakingrecipes/MainActivity.java
+++ app/src/main/java/com/sommerengineering/bakingrecipes/MainActivity.java
@@ -1,5 +1,8 @@
 package com.sommerengineering.bakingrecipes;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // check status of internet connectivity
+    private boolean isConnected() {
+
+        // get internet connectivity status as a boolean
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // get network connection metadata
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+
+        // boolean representing internet is connected, or in progress connecting
+        return (activeNetwork != null) && activeNetwork.isConnectedOrConnecting();
     }
 }
