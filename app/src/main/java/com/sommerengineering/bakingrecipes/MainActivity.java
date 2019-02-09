0e341be5a1e1e9e8b0f6ac83eb56b4e9fa16cbe1
Drew Sommer <andrew.p.sommer@gmail.com>
2019-02-08 16:28:04 -0800

    Test network connection and log JSON payload

--- app/src/main/java/com/sommerengineering/bakingrecipes/MainActivity.java
+++ app/src/main/java/com/sommerengineering/bakingrecipes/MainActivity.java
@@ -1,13 +1,16 @@
 package com.sommerengineering.bakingrecipes;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import java.net.URL;
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +22,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        URL url = Utilities.getUdacityUrl();
+        final URL url = Utilities.getUdacityUrl();
         Log.e(LOG_TAG, "~~ " + url.toString());
+
+        new Thread(new Runnable() {
+            public void run() {
+                String responseJson = Utilities.getResponseFromHttp(url);
+                Log.e(LOG_TAG, "~~ " + responseJson);
+            }
+        }).start();
+
     }
 
     // check status of internet connectivity
--- app/src/main/java/com/sommerengineering/bakingrecipes/Utilities.java
+++ app/src/main/java/com/sommerengineering/bakingrecipes/Utilities.java
@@ -1,9 +1,14 @@
 package com.sommerengineering.bakingrecipes;
 
 import android.net.Uri;
+import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 // final access modifier because no objects of this class will ever be created
 // this class is a holder for various static methods related to the network connection
@@ -13,7 +18,7 @@ public final class Utilities {
     private static final String LOG_TAG = Utilities.class.getSimpleName();
 
     // URL constants
-    private static final String UDACITY_BASE_URL = "http://go.udacity.com/android-baking-app-json";
+    private static final String UDACITY_BASE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
 
     // create a URL for the given Udacity JSON server
     static URL getUdacityUrl() {
@@ -27,7 +32,41 @@ public final class Utilities {
         }
 
         return url;
+    }
 
+    // obtain JSON response string from API endpoint
+    public static String getResponseFromHttp(URL url){
+
+        // initialize response to null
+        String responseJson = null;
+
+        // HTTP connections can throw IOException
+        try {
+
+            // open the network connection
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            // create an input stream from the opened connection
+            InputStream inputStream = urlConnection.getInputStream();
+
+            // "\\A" delimiter denotes the end of server response
+            // therefore the entire response is read in one shot
+            Scanner scanner = new Scanner(inputStream);
+            scanner.useDelimiter("\\A");
+
+            // check if there is anything in the stream
+            if (scanner.hasNext()) {
+                responseJson = scanner.next();
+            }
+
+            // close the connection
+            urlConnection.disconnect();
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "~~ " + e.toString());
+        }
+
+        return responseJson;
     }
 
 }
