ea43dbf498caa9065d05d58d54e1bc151a729952
Drew Sommer <andrew.p.sommer@gmail.com>
2019-02-11 22:26:53 -0800

    Launcher icon; minor layout changes

--- app/src/main/java/com/sommerengineering/bakingrecipes/DetailActivity.java
+++ app/src/main/java/com/sommerengineering/bakingrecipes/DetailActivity.java
@@ -7,4 +7,6 @@ public class DetailActivity extends AppCompatActivity {
     // simple tag for log messages
     private static final String LOG_TAG = DetailActivity.class.getSimpleName();
 
+    // TODO extract Dessert from intent, loop through ingredients and steps to create new views dynamically
+
 }
