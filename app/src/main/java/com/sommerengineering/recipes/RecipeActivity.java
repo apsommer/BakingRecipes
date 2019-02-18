20c230dbbe8e0915577b9e6296bb334482acf418
Drew Sommer <andrew.p.sommer@gmail.com>
2019-02-18 15:59:40 -0800

    Two-pane UI has a comfortable 1/3 - 2/3 visual proportion

--- app/src/main/java/com/sommerengineering/recipes/RecipeActivity.java
+++ app/src/main/java/com/sommerengineering/recipes/RecipeActivity.java
@@ -5,11 +5,26 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RecipeActivity extends AppCompatActivity implements DetailFragment.OnStepClickListener {
 
     // simple tag for log messages
     private static final String LOG_TAG = RecipeActivity.class.getSimpleName();
+
+    // flag for two-pane UI (tablet) or single pane UI (phone)
+    private boolean mIsTwoPane;
+
+    // bind views using Butterknife library
+    @BindView(R.id.fl_detail_container) FrameLayout mRecipeContainer;
+    @BindView(R.id.fl_step_container) FrameLayout mStepContainer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,6 +32,9 @@ public class RecipeActivity extends AppCompatActivity implements DetailFragment.
         // inflate the fragment container
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        // initialize the Butterknife library
+        ButterKnife.bind(this);
 
         // extract dessert from the main activity intent
         Intent intent = getIntent();
@@ -30,9 +48,39 @@ public class RecipeActivity extends AppCompatActivity implements DetailFragment.
         DetailFragment detailFragment = new DetailFragment();
         detailFragment.setArguments(bundle);
 
-        // create the DetailFragment
+        // get reference to the Android fragment manager
         FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().add(R.id.recipe_container, detailFragment).commit();
+
+        // single pane layout
+        if (mRecipeContainer == null) {
+
+            // set flag
+            mIsTwoPane = false;
+
+            // create the detail fragment showing ingredients and step metadata
+            fragmentManager.beginTransaction().add(R.id.fl_detail_and_step_container, detailFragment).commit();
+
+        // two-pane layout
+        } else {
+
+            // set flag
+            mIsTwoPane = true;
+
+            // detail fragment always occupies 1/3 of screen width
+            // get current device width in dp
+            int dpWidth = getResources().getDisplayMetrics().widthPixels;
+
+            // layout size and position are defined in LayoutParams
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    dpWidth/3, ViewGroup.LayoutParams.MATCH_PARENT);
+
+            // bind these layout parameters to the framelayout
+            mRecipeContainer.setLayoutParams(layoutParams);
+
+            // create the recipe fragment showing ingredients and step metadata
+            fragmentManager.beginTransaction().add(R.id.fl_detail_container, detailFragment).commit();
+        }
+
     }
 
     @Override
@@ -44,12 +92,20 @@ public class RecipeActivity extends AppCompatActivity implements DetailFragment.
         bundle.putInt("selectedStepId", stepId);
 
         // instantiate a new StepFragment  and add the bundle
-        StepFragment playerFragment = new StepFragment();
-        playerFragment.setArguments(bundle);
+        StepFragment stepFragment = new StepFragment();
+        stepFragment.setArguments(bundle);
 
-        // create the StepFragment
+        // get reference to the Android fragment manager
         FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.recipe_container, playerFragment).commit();
+
+        // single pane layout: step fragment replaces the recipe fragment
+        if (!mIsTwoPane) {
+            fragmentManager.beginTransaction().replace(R.id.fl_detail_and_step_container, stepFragment).commit();
+
+        // two-pane layout: recipe fragment and step fragment shown simultaneously
+        } else {
+            fragmentManager.beginTransaction().replace(R.id.fl_step_container, stepFragment).commit();
+        }
 
     }
 
